3c8fd4adb25c66571b4e75492ec2803771dd7de6
Benstaw <ben@2broadview.com>
2021-10-09 19:12:09 -0400

    unfinished ics handling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,14 +24,100 @@ const loadEvents = function(){
         for(let count = 0; count < json.length; count++){
             let tr = document.createElement("tr")
             let td = document.createElement("td")
+            let td2 = document.createElement("td")
             let item = document.createTextNode(json[count].eventName)
             td.appendChild(item)
             tr.appendChild(td)
+            
+            item = document.createElement("button")
+            item.innerHTML = "Click for ics"
 
+            //initializer(json[count])            
+
+
+            td2.appendChild(item)
+            tr.appendChild(td2)
+            item.href = createFile(json[count])
+            item.download = "event.ics"
+            
             table.append(tr)
+            item.click()
+
         }
       })
 }
+
+const initializer = function(event) {
+    let startDate = event.availableDates[0] = new Date();
+    let endDate = event.availableDates[event.availableDates.length - 1] = new Date();
+    endDate = event.availableDates[event.availableDates.length - 1].min = new Date()
+      .toISOString()
+      .substr(0, 10);
+    //startDate.addEventListener("change", function() {
+      //let val = this.value;
+      //endDate.min = new Date(val)
+        //.toISOString()
+        //.substr(0, 10);
+    //});
+  };
+
+  const createFile = function(event) {
+    let eventDate = {
+        start: event.availableDates[0],
+        end: event.availableDates[event.availableDates.length - 1]
+      },
+      summary = event.eventName,
+      description = event.description
+     return makeIcsFile(eventDate, summary, description);
+    //downloadButton.classList.remove("hide");
+  }
+  function convertDate(date) {
+    var event = new Date(date).toISOString();
+    event = event.split("T")[0];
+    event = event.split("-");
+    event = event.join("");
+    return event;
+  }
+  function makeIcsFile(date, summary, description) {
+    let icsFile = null;
+    let test =
+      "BEGIN:VCALENDAR\n" +
+      "CALSCALE:GREGORIAN\n" +
+      "METHOD:PUBLISH\n" +
+      "PRODID:-//Test Cal//EN\n" +
+      "VERSION:2.0\n" +
+      "BEGIN:VEVENT\n" +
+      "UID:test-1\n" +
+      "DTSTART;VALUE=DATE:" +
+      convertDate(date.start) +
+      "\n" +
+      "DTEND;VALUE=DATE:" +
+      convertDate(date.end) +
+      "\n" +
+      "SUMMARY:" +
+      summary +
+      "\n" +
+      "DESCRIPTION:" +
+      description +
+      "\n" +
+      "END:VEVENT\n" +
+      "END:VCALENDAR";
+  
+    let data = new File([test], { type: "text/plain" });
+  
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (icsFile !== null) {
+      window.URL.revokeObjectURL(icsFile);
+    }
+  
+    icsFile = window.URL.createObjectURL(data);
+  
+    return icsFile;
+  }
+
+  
+  
 
 window.onload = function(){
     loadEvents()
